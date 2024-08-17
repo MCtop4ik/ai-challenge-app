7bb9a183d14063c115d9881b8e4af1a38a960899
Zetni <azakharov@workmovr.com>
2024-08-17 23:27:31 +0400

    fix: colors, fix: positioning of title and logo

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html style={{ backgroundColor: 'aqua' }}>
+    <Html style={{ backgroundColor: "rgb(1, 47, 108)"}}>
       <Head>
       </Head>
       <body>
--- pages/components/header/header.component.js
+++ pages/components/header/header.component.js
@@ -3,7 +3,7 @@ import styles from './header.module.css';
 export default function Header() {
     return (
         <div className={styles.header}>
-            <img className={styles.header__logo} src="logo.png"></img>
+            <img className={styles.header__logo} src="logo-white.png"></img>
         </div>
     )
 }
