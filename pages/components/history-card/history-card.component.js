66e1578460d5ddc5c6b9cfe03e1e3c29e64135bf
Zetni <azakharov@workmovr.com>
2024-08-17 20:51:03 +0400

    feat: add card sample

--- pages/components/history-card/history-card.component.js
+++ pages/components/history-card/history-card.component.js
@@ -3,8 +3,14 @@ import styles from './history-card.module.css';
 export default function HistoryCard({id}) {
     return (
     <div className={styles.historyCard}>
-        <h2 className={styles.historyCard__title}>Card {id}</h2>
-        <h3 class="historyCard__id">{id}</h3>
+        <div className={styles.historyCard__information}>
+            <h2 className={styles.historyCard__information_date}>Card {id}</h2>
+            <h2 className={styles.historyCard__information_amount}>Card {id}</h2>
+        </div>
+        <div className={styles.historyCard__photo_container}>
+            <img className={styles.historyCard__photo_container_photo} src="./input.png"></img>
+        </div>
+        {/* <h3 class="historyCard__id">{id}</h3> */}
     </div>
     )
 }
