ee4fe2f11e2df9a57cd24f90b6dd050381f3c95b
Zetni <azakharov@workmovr.com>
2024-08-19 01:51:57 +0400

    chore: rename history in recent images

--- pages/components/history/history.component.js
+++ pages/components/history/history.component.js
@@ -4,7 +4,7 @@ import styles from './history.module.css';
 export default function History() {
     return (
     <div className={styles.historyContainer}>
-        <h1 className={styles.historyContainer__title}>History</h1>
+        <h1 className={styles.historyContainer__title}>Recent Images</h1>
         <HistoryCards></HistoryCards>
     </div>
     )
