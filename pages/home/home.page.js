e9f907bc9a483830151c8c6ba8d468af04732cc8
Zetni <azakharov@workmovr.com>
2024-08-23 02:32:53 +0400

    feat: add styles for camera in modal

--- pages/home/home.page.js
+++ pages/home/home.page.js
@@ -2,7 +2,6 @@ import Header from "./../components/header/header.component";
 import Buttons from "./../components/buttons/buttons.component";
 import Separator from "./../components/separator/separator.component";
 import History from "./../components/history/history.component";
-import Camera from "./../components/camera/camera.component";
 
 export default function Home() {
     return (
