c25f3c1b97f35bf9b0b3bae6721f14b83e6fd87a
Zetni <azakharov@workmovr.com>
2024-08-23 18:34:18 +0400

    feat: create idb service

---
+++ pages/api/idb.api.js
@@ -0,0 +1,52 @@
+import { openDB } from 'idb';
+
+const DB_NAME = 'imageDB';
+const STORE_NAME = 'images';
+const DB_VERSION = 1;
+
+class IDBApi {
+  constructor() {
+    this.dbPromise = openDB(DB_NAME, DB_VERSION, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains(STORE_NAME)) {
+          db.createObjectStore(STORE_NAME);
+        }
+      },
+    });
+  }
+
+  async saveImage(base64Image, key = 'myImage') {
+    const db = await this.dbPromise;
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    await store.put(base64Image, key);
+    await tx.done;
+  }
+
+  async getImage(key = 'myImage') {
+    const db = await this.dbPromise;
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    const base64Image = await store.get(key);
+    await tx.done;
+    return base64Image;
+  }
+
+  async deleteImage(key = 'myImage') {
+    const db = await this.dbPromise;
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    await store.delete(key);
+    await tx.done;
+  }
+
+  async clearAll() {
+    const db = await this.dbPromise;
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    await store.clear();
+    await tx.done;
+  }
+}
+
+export default new IDBApi();
