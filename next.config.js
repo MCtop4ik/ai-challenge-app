83e470e49a5e510f437dd871c878b64a8b0ea804
Zetni <azakharov@workmovr.com>
2024-10-02 16:45:57 +0400

    feat: create pwa

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    disable: process.env.NODE_ENV === "development",
-    workboxOptions: {
-      disableDevLogs: true,
-    },
-    // ... other options you like
+// next.config.js
+const withPWA = require('next-pwa')({
+    dest: 'public', // Folder to store generated service worker and PWA files
+    disable: process.env.NODE_ENV === 'development', // Disable in development mode
   });
   
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    // ... other options you like
-  };
-  
-  module.exports = withPWA(nextConfig);
-  
+  module.exports = withPWA({
+    // Other Next.js config options can go here
+  });
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html style={{ backgroundColor: "rgb(1, 47, 108)"}}>
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </Head>
       <body>
         <Main />
